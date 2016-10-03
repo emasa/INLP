eb1828faf35a4dbe213699a0e9a786a8c3824a04
monete <monete@gmail.com>
2016-10-03 19:37:55 +0200

    Primer_commit

--- hello_world.py
+++ hello_world.py
@@ -6,24 +6,31 @@ def tokenizeByWhiteSpaces():
     #This function will tokenize the text into words
     #We will extract words by lowercase and delete punctuation using the string function
     f = open('en.txt')
-    #raw = f.read().lower().translate(None, string.punctuation)
-    raw = f.read()
+    raw = f.read().lower().translate(None, string.punctuation)
     #tokenizeing by whitespaces and lowercase it gives exactly the same number of words than auxiliar.py provided by Horacio so we assume it's the correct number of tokens (words)
     tokens = WhitespaceTokenizer().tokenize(raw)
-
     #count total number of tokens
     total_tokens = len(tokens)
     #count number of unique tokens
     unique_tokens = len(set(tokens))
     #create frquency distribution table and plot it
     fdist1 = nltk.FreqDist(tokens)
-    #fdist1.plot(50, cumulative=False)
+    fdist1.plot(50, cumulative=False)
     print len(raw), 'chars read'
     print len(tokens), 'tokens extracted'
     print len(fdist1), 'tokens unique'
-    print len(set(tokens)), 'set of tokens'
     print fdist1.most_common(50)
+    #print the 50 most frequent words
+    for word, frequency in fdist1.most_common(50):
+        print(u'{};{}'.format(word, frequency))
 
+    for word, frequency in fdist1.most_common(50):
+        frequency_total = frequency / len(fdist1)
+        print frequency_total
+
+    print unique_tokens/total_tokens, 'unicos/totales'
+#   print fdist1(10)
+#   print fdist1(1)/total_tokens
 #def frequencies():
 #count of that word divided by the total number of samples
 
