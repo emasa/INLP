6c9180c1a0a46a3e01ef43ac05e2497aee550cb2
monete <monete@gmail.com>
2016-10-03 12:17:06 +0200

    Primer_commit

--- hello_world.py
+++ hello_world.py
@@ -1,23 +1,32 @@
-import nltk
-from nltk import *
-from nltk.tokenize import word_tokenize
+from __future__ import division
+from nltk.tokenize import WhitespaceTokenizer
+import nltk, re, pprint
 
 def tokenizeText():
-    var = open('en.txt')
-    var2 = nltk.word_tokenize(var)
-    #print len(var), 'tokens read'
+#open text file and load it in a variable
+    f = open('en.txt')
+    raw = f.read()
+    print len(raw), 'chars read'
+    #tokenize text from file
+    #tokens = nltk.word_tokenize(raw)
+    unset = set(raw)
+    print len(unset) , ' sets extracted'
+#tokenize by whitespaces and lowercase it gives exactly the same number of words than auxiliar.py
+    tokens = WhitespaceTokenizer().tokenize(raw.lower())
+    print len(tokens), 'tokens extracted'
+    #count total number of tokens
+    total_tokens = len(tokens)
+    #count number of unique tokens
+    unique_tokens = len(set(tokens))
+    #create frquency distribution table and plot it
+    fdist1 = nltk.FreqDist(tokens)
+ #   fdist1.plot(50, cumulative=False)
+    print len(fdist1), 'tokens unique'
+    print len(set(tokens)), 'set of tokens'
+    print fdist1.most_common(10)
 
-
-def importingBrownCorpusFromNLTK(outF):
-    # type: (object) -> object
-    "importing tagged brown corpus from NLTK and writing on a file OutF"
-    outF = open(outF,'w')
-    from nltk.corpus import brown
-    brown_news_tagged = brown.tagged_words(categories='news')
-    print 'size', len(brown_news_tagged)
-    for i in brown_news_tagged:
-        outF.write(i[0]+'\t'+i[1]+'\n')
-    outF.close()
+def frequencies():
+    #count of that word divided by the total number of samples
 
 
 
@@ -30,6 +39,7 @@ def getWordsFromFile(inF):
             words.append(word)
     print len(words), 'words read'
     return words
+    print(words)
 
-#getWordsFromFile('en.txt')
+getWordsFromFile('en.txt')
 tokenizeText()
