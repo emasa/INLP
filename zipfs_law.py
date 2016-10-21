73f35ccb02dad319cefbd940c6af1200e339f1cd
emasa <emalomas@gmail.com>
2016-10-21 23:50:59 +0200

    Added helper classes ZIpfsLawVerifier, ZIpfsLawCharacterWiseVerifier to compute frequency, K and rank on a given corpus. Added plotZipfsLaw to produce serveral plots. The main function recieves 3 parameters: to configure words or characters, to enable the use of lemmas (no compatible with the spanish corpus) and to choose the corpus file. Pending tasks: better docs (according to the guidelines) and organize code in differents files (also according to the guidelines).

--- zipfs_law.py
+++ zipfs_law.py
@@ -1,97 +1,205 @@
+# encoding: utf-8
 from __future__ import division
+
 from nltk.tokenize import WhitespaceTokenizer
+from nltk.stem import WordNetLemmatizer
 import nltk, re, pprint, string
 
-def tokenizeByWhiteSpaces(rawtext):
-    #This function will tokenize the text into words
-    #tokenizeing by whitespaces and lowercase it gives exactly the same number of words than auxiliar.py provided by Horacio so we assume it's the correct number of tokens (words)
-    tokens = WhitespaceTokenizer().tokenize(rawtext)
-    return tokens
+import numpy as np
+import matplotlib.pyplot as plt
 
-def distributionList(tokens):
-    #This function will create the frequency distribution of the tokens
-    fdist1 = nltk.FreqDist(tokens)
-  #  fdist1.plot(50)
-    return fdist1
+def plotZipfsLaw(kList, freqList, maxN=None, minN=None):
 
-def expectedZipLawCurve(fdist1):
-    import numpy as np
-    import matplotlib.pyplot as plt
-    MaxFreq=fdist1[fdist1.max()]
-    Z = np.random.zipf(2, 50)
-    index=1
-    # for sample in fdist1:
-    #    MaxFreq = MaxFreq / index
-    #    np.append(MaxFreq)
-    #    index +=1
-  #  plt.plot(sorted(np[:50],reverse=True))
-    plt.plot(sorted(Z,reverse=True))
+    minN = minN if minN else 0
+    maxN = maxN if maxN else len(kList)
+
+    if minN != 0 or maxN != len(kList):
+        kList = kList[minN:maxN]
+        freqList = freqList[minN:maxN]
+
+    logKList    = np.log10(kList)
+    logFreqList = np.log10(freqList)
+
+    if minN == 0:
+        minN =  1
+        maxN += 1
+
+    rankList = range(minN, maxN)
+    logRankList = np.log10(rankList)
+
+    fig = plt.figure()
+    plt.plot(logRankList, logFreqList)
+    fig.suptitle("Zipf's Law")
+    plt.xlabel("log(rank)")
+    plt.ylabel("log(frequency)")
+
+    fig = plt.figure()
+    plt.plot(rankList, freqList)
+    fig.suptitle("Zipf's Law")
+    plt.xlabel("rank")
+    plt.ylabel("frequency")
+
+    fig = plt.figure()
+    plt.plot(rankList, kList)
+    fig.suptitle("Zipf's Law")
+    plt.xlabel("rank")
+    plt.ylabel("K")
+
+    fig = plt.figure()
+    plt.plot(logRankList, logKList)
+    fig.suptitle("Zipf's Law")
+    plt.xlabel("log(rank)")
+    plt.ylabel("log(K)")
+
     plt.show()
 
-def computeZipfsLaw(fdist1):
-    import numpy as np
-    import matplotlib.pyplot as plt
-    index = 1
-    K = []
-    freq = []
-    rank = []
-    logFreq = []
 
-    for word, frequency in fdist1.most_common():
-        #frequency_total = frequency / len(tokens)
-        #frequency_total.update({word:frequency / len(tokens)})
-        #frequency_total = np.append(word,frequency/len(tokens))
-        #frequency_total = fdist1.freq(word)
-        #f=K/r so K = f.r
-        K.append(index * frequency)
-        freq.append(frequency)
-        rank.append(index)
+class ZipfsLawVerifier(object):
 
-        index  += 1
-   # plt.plot(K)
-  #  plt.show()
-    logK = np.log10(K)
-    logFreq = np.log10(freq)
-    logRank = np.log10(rank)
-    plt.plot(logFreq)
-    #plt.axis([np.amin(logRank), np.amax(logRank), np.amin(logFreq), np.amax(logFreq)])
-    plt.xscale('log')
-    plt.show()
-    #print (K[:1000])
-    #print (logK[:1000])
-def counters():
-    # count total number of tokens
-    total_tokens = len(tokens)
-    unique_tokens = len(set(tokens))
-    print len(raw), 'chars read'
-    print len(tokens), 'tokens extracted'
-    print len(fdist), 'tokens unique'
-    print fdist.most_common(50)
-    # count number of unique tokens
-    #frequency_total.plot(50)
-        #print frequency_total, 'frequenc ', frequency, 'total ', len(tokens)
+    def __init__(self, with_lemmas=False):
+        # config
+        self._with_lemmas = with_lemmas
+        # internal representation
+        self._tokenized_corpus = ""
+        self._tokens_sorted_by_rank = []
+        self._tokens_to_rank_map = {}
+        self._freqList = []
+        self._kList = []
+        self._freq_dist = None
 
-   # print unique_tokens/total_tokens, 'unicos/totales'
-   # print len(frequency_total)
-#   print fdist1(10)
-#   print fdist1(1)/total_tokens
-#def frequencies():
-#count of that word divided by the total number of samples
+    def process_corpus(self, raw_corpus):
+        cleaned_corpus = self._preprocessing(raw_corpus)
+        self._tokenized_corpus = self._tokenize(cleaned_corpus)
+        self._computeFrequencyDistribution()
 
-#getWordsFromFile('es.txt')
-f = open('en.txt')
-# We will extract words by lowercase and delete punctuation using the string function
-#get a list of the stopwords
-#stp = nltk.corpus.stopwords.words('english')
-#from your text of words, keep only the ones NOT in stp
-#filtered_text = [w for w in f if not w in stp]
-raw = f.read().lower().translate(None, string.punctuation)
-tokens = tokenizeByWhiteSpaces(raw)
-fdist = distributionList(tokens)
-expectedZipLawCurve(fdist)
-computeZipfsLaw(fdist)
-#counters()
-#constructs an array with the list of words
-#wlist=[]
-#wlist.append(fdist.keys())
-#print len(wlist)
+    def _preprocessing(self, input_text):
+        # delete line breaks
+        output_text = input_text.translate(None, "\n")
+        # delete punctuation
+        output_text = output_text.translate(None, string.punctuation)
+        # delete digits
+        output_text = output_text.translate(None, string.digits)
+        # translate from upper to lower case
+        output_text = output_text.lower()
+
+        return output_text
+
+    def _tokenize(self, input_text):
+        # This function will tokenize the text into words
+        tokens = WhitespaceTokenizer().tokenize(input_text)
+
+        # optional: test with lemmas
+        if self._with_lemmas:
+            lemmatizer = WordNetLemmatizer()
+            tokens = [ lemmatizer.lemmatize(token) for token in tokens]
+
+        return tokens
+
+    def _computeFrequencyDistribution(self):
+        self._freq_dist = nltk.FreqDist(self._tokenized_corpus)
+
+        # split sorted tokens and frequency in 2 list
+        self._tokens_sorted_by_rank, self._freqList = zip(*self._freq_dist.most_common())
+
+        # compute K=rank * freq
+        self._kList = [(rank_idx+1) * self._freqList[rank_idx] for rank_idx in range(self.B())]
+        # mapping (token, rank)  useful for individual queries
+        self._tokens_to_rank_map = dict((self._tokens_sorted_by_rank[rank_idx], rank_idx+1) for rank_idx in range(self.B()))
+
+    def rank(self, token):
+        assert token in self._tokens_to_rank_map
+
+        return self._tokens_to_rank_map[token]
+
+    def K(self, token=None):
+        # if no parameter is provided, returns the full list sorted by rank
+        if token is None:
+            return list(self._kList)
+
+        idx = self.rank(token) - 1
+        return self._kList[idx]
+
+    def freq(self, token=None):
+        # if no parameter is provided, returns the full list sorted by rank
+        if token is None:
+            return list(self._freqList)
+
+        idx = self.rank(token) - 1
+        return self._freqList[idx]
+
+    def nthToken(self, n):
+        assert 0 < n <= self.B()
+
+        idx = n-1
+        return self._tokens_sorted_by_rank[idx]
+
+    def N(self):
+        return self._freq_dist.B()
+
+    def B(self):
+        return self._freq_dist.B()
+
+class ZipfsLawCharacterWiseVerifier(ZipfsLawVerifier):
+    def _tokenize(self, input_text):
+        output_text = input_text.translate(None, " ")
+
+        # split the text into a list of characters
+        tokens = list( output_text )
+        return tokens
+
+def main(*args, **kwargs):
+    """
+    Main function.
+    """
+    # open the corpus path passed by parameter and store its content as raw text
+    with open(kwargs["corpus_path"]) as corpus_file:
+        raw_text_corpus = corpus_file.read()
+
+    # Process the corpus
+    # if with_words is True, verifies Zipfs Law on words, otherwise on characters.
+    with_words = kwargs.get("with_words", True)  # default: words
+    if with_words:
+        # if with_lemmas is True, verifies Zipfs Law on lemmas, otherwise on simple words.
+        with_lemmas = kwargs.get("with_lemmas", False)  # default: without lemmas
+        verifier = ZipfsLawVerifier(with_lemmas=with_lemmas)
+    else:
+        verifier = ZipfsLawCharacterWiseVerifier()
+
+    verifier.process_corpus(raw_text_corpus)
+
+    # Compute average and std deviation of K (expected ZipfsLaw's constant)
+    K = verifier.K()
+    kAverage = np.average(K)
+    kStdDeviation = np.std(K)
+    print "Average K: %s ; Standard Deviation K: %s" % (kAverage, kStdDeviation)
+
+    frequency = verifier.freq()
+
+    # Print top N elements in the ranking
+    topN = min(verifier.B(), 50) # default 50
+    print "Top %s elements in the ranking" % (topN,)
+    print "{:^8} {:^8} {:^8} {:^8}".format("Rank", "Token", "K", "Frequency")
+    for rank in range(1, topN + 1):
+        token = verifier.nthToken(rank)
+        print "{:^8} {:^8} {:^8} {:^8}".format(rank, token, verifier.K(token), verifier.freq(token))
+
+    # Plots (ranking range can be modified)
+    minN, maxN = 0, verifier.B()
+    plotZipfsLaw(K, frequency, minN=minN, maxN=maxN)
+
+
+if __name__ == '__main__':
+    # Note: choose between english/spanish corpuses and between options words/single characters
+
+    # english corpus
+    main(corpus_path='en.txt', with_lemmas=True)
+
+    # spanish corpus
+    # the wordnet lemmas feature don't support non-ascii characters (such as á, é, etc)
+    #main(corpus_path='es.txt')
+
+    # english corpus with characters instead of words
+    #main(corpus_path='en.txt', with_words=False)
+
+    # spanish corpus with characters instead of words
+    #main(corpus_path='es.txt', with_words=False)
